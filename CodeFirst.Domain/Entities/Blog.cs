83dcc4a7459474de722b456a107edb41dbccbdaf
matosauer <livepictures@gmail.com>
2026-03-01 07:36:59 +0200

    State tests

--- CodeFirst.Domain/Entities/Blog.cs
+++ CodeFirst.Domain/Entities/Blog.cs
@@ -1,11 +1,19 @@
 ﻿namespace CodeFirst.Domain.Entities
 {
+    public enum State
+    {
+        Draft,
+        Published, 
+    }
+
     public class Blog
     {
         public Guid BlogId { get; set; }
         public required string Name { get; set; }
 
         public string? Description { get; set; }
+
+        public State State { get; set; } = State.Draft;
 
         public ICollection<Post> Posts { get; set; } = [];
     }
